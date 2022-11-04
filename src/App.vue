07096992d0c370747bc85ffc8a8224f211bdcc81
AlessandroVianaSilva <silvaalessandro352@gmail.com>
2022-11-04 09:02:27 -0300

    projeto finalizado

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="calculadora">
+    <Calculadora/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calculadora from './components/Calculadora.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Calculadora,
+  },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-color: #222;
+  margin: 0;
+  padding: 0;
+}
+#calculadora{
+  font-family: 'Poppins', sans-serif;
+  font-size: 1rem;
 }
 </style>
